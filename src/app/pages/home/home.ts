415810dcb1c7d624b12f56334f4b79a506b13861
Roogry <jodiemantra@gmail.com>
2025-12-07 21:07:11 +0700

    feat: introduce redesigned home page with hero section, account cards, and awards, alongside a conditional header border.

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -8,5 +8,33 @@ import { NgOptimizedImage } from '@angular/common';
   styleUrl: './home.css',
 })
 export class Home {
+  awards = [
+    {
+      title: '#3 most downloaded finance app',
+      image: 'assets/images/awards/mobile-store.png',
+    },
+    {
+      title: '4.6 out of 5 on Trustpilot',
+      image: 'assets/images/awards/trustpilot.png',
+    },
+    {
+      title: 'Best International Payments Provider 2025',
+      image: 'assets/images/awards/best-payment.png',
+    },
+    {
+      title: 'Best Consumer Banking Mobile App 2025',
+      image: 'assets/images/awards/best-fintech.png',
+    },
+  ];
 
+  awardBadges = [
+    {
+      title: 'Customer Satisfaction — Gold',
+      image: 'assets/images/awards/cust-satisfaction.png',
+    },
+    {
+      title: 'Consumer Guardian Badge 2025',
+      image: 'assets/images/awards/cons-guardian.png',
+    },
+  ];
 }
